2d8d6510c6665189b276e93b84006ffc13815bcd
+mitchellengetich <+mitchellngetich24@gmail.com>
2025-06-23 19:43:19 +0300

    update profile_profilepage

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import ProfilePage from './pages/ProfilePage';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<ProfilePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
